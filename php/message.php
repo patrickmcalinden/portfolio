f8a39492b1bd90b185154add637145304eb638c4
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-11 04:03:56 -0400

    main

--- php/message.php
+++ php/message.php
@@ -1,26 +1,24 @@
+//Contact Form in PHP
 <?php
-
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phonenumber = $_POST['phonenumber'];
-$message = $_POST['message'];
-
-
-if(!empty($email) && !empty($message)){
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $message = htmlspecialchars($_POST['message']);
+  if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-        $reciever = "patrickmcalinden226@gmail.com";
-        $subject = "From: $name <$email>";
-        $body = "Name: $name\nEmail: $email\nPhonenumber: $phonenumber\n\nMessage: $message";
-        $sender = "From: $email";
-        if(mail($reciever, $subject, $body, $sender)){
-            echo "Message sent!";
-        }else{
-            echo "Failed to sened message!";
-        }
+      $receiver = "patrickmcalinden226@gmail.com";
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
     }else{
-        echo "Enter a valid Email!";
+      echo "Enter a valid email address!";
     }
-}else{
-    echo "Email and Message fields are required!";
-}
+  }else{
+    echo "Email and message field is required!";
+  }
 ?>
