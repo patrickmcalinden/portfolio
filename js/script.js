213c73363f4304153f74f44fb901dc4dc75c827f
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-11 05:02:57 -0400

    main

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ form,onsubmit = (e) =>{
     
     let xhr = new XMLHttpRequest();
 
-    xhr.open("POST", "message.php", true);
+    xhr.open("message.php", true);
     xhr.onload = ()=>{
         if(xhr.readyState== 4 && xhr.status == 200){
             let response = xhr.response;
