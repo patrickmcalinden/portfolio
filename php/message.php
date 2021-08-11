96fd620c6fc7c04bc2f429b54eeaa1057c05d719
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-11 06:06:51 -0400

    main

---
+++ php/message.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST['sumbit'])){
+    $name = $_POST['contact-name'];
+    $email = $_POST['contact-email'];
+    $phone = $_POST['contact-phone'];
+    $message = $_POST['contact-message'];
+
+
+    $mailto = "patrickmcalinden226@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You got an email form".$name.".\n\n".$message;
+
+    mail($mailto, $subject, $txt, $headers);
+    header("Location: index/email-sent");
+
+}
+
+?>
