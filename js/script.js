9aa2f1d8f5846478c0e9acbc8917c47964fd24f6
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-11 02:12:17 -0400

    main

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
+const form = document.querySelector("form")
 
-const form = document.querySelector("form"),
+form.onsubmit = (e) =>{
+    let xhr = new XMLHttprequest();
+    xhr.open("POST", "message.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState== 4 && xhr.status == 200){
+            let response = xhr.response;
+            if(repsone.indexOf("Email and Message fields need to be filled") != -1 || response.indexOf("Enter a valid Email!") != -1 || response.indexOf("Failed to sened message!")){
+            }else{
+                form.reset();
+                setTimeout(()=>{
 
-form,onsubmit = (e)=>{
-  e.preventDefault();
-  form.classList.add("disabled");
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("Failed") != -1){
-      }else{
-        form.reset();
-        setTimeout(()=>{
-        }, 3000);
-      }
-      form.classList.remove("disabled");
+                },3000);
+        }            
+
+        }
     }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
+    let formData = new FormData();
+    xhr.send(formData);
 }
