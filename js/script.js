408d427db761436a8f20e9ff6c7a61fca5b64675
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-12 17:27:14 -0400

    main

--- js/script.js
+++ js/script.js
@@ -3,9 +3,6 @@ const form = document.querySelector("form"),
 statusTxt = form.querySelector(".status");
 form.onsubmit = (e)=>{
   e.preventDefault();
-  statusTxt.style.color = "Red";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
   form.classList.add("disabled");
   let xhr = new XMLHttpRequest();
   xhr.open("POST", "message.php", true);
@@ -13,11 +10,11 @@ form.onsubmit = (e)=>{
     if(xhr.readyState == 4 && xhr.status == 200){
       let response = xhr.response;
       if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
-        statusTxt.style.color = "red";
+        
       }else{
         form.reset();
         setTimeout(()=>{
-          statusTxt.style.display = "none";
+          
         }, 3000);
       }
       statusTxt.innerText = response;
