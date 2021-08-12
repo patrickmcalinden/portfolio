c6cdba3fcce7063872f8bfba2b054d9beecc2b97
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-12 00:00:37 -0400

    main

--- php/message.php
+++ php/message.php
@@ -1,15 +1,24 @@
+//Contact Form in PHP
 <?php
-    $userName = $_POST['contact-name'];
-    $userEmail = $_POST['contact-email'];
-    $userPhone = $_POST['contact-phone'];
-    $message = $_POST['contact-message'];
-
-    $to = "patrickmcalinden226@gmail.com";
-    $body = "";
-
-    $body .= "From: ".$userName. "\r\n";
-    $body .= "Email: ".$userEmail. "\r\n";
-    $body .= "Message: ".$message. "\r\n";
-
-    mail($to,"New Email",$body);
+  $name = htmlspecialchars($_POST['contact-name']);
+  $email = htmlspecialchars($_POST['contact-email']);
+  $phone = htmlspecialchars($_POST['contact-phone']);
+  $message = htmlspecialchars($_POST['message']);
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = "patrickmcalinden226@gmail.com"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
 ?>
