24098884fcd4f9c384500d8123ab9690ac67cba8
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-12 18:13:01 -0400

    main

--- message.php
+++ message.php
@@ -1,14 +1,16 @@
 //Contact Form in PHP
 <?php
-  $name = htmlspecialchars($_POST['contact-name']);
-  $email = htmlspecialchars($_POST['contact-email']);
-  $phone = htmlspecialchars($_POST['contact-phone']);
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $phone = htmlspecialchars($_POST['phone']);
+  $website = htmlspecialchars($_POST['website']);
   $message = htmlspecialchars($_POST['message']);
+
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "patrickmcalinden226@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
@@ -19,6 +21,6 @@
       echo "Enter a valid email address!";
     }
   }else{
-    echo "Email and message fields is required!";
+    echo "Email and message field is required!";
   }
 ?>
