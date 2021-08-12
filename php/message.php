1bd3db19f2efa71bde2d9f782fcc90b6f431bc63
Patrick McAlinen <patrickmcalinden226@gmail.com>
2021-08-11 23:41:02 -0400

    main

--- php/message.php
+++ php/message.php
@@ -1,15 +1,23 @@
-
 <?php
     ini_set( 'display_errors', 1 );
     error_reporting( E_ALL );
-    $from = "test@hostinger-tutorials.com";
-    $to = "patrickmcalinden226@gmail.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    if(mail($to,$subject,$message, $headers)) {
-		echo "The email message was sent.";
-    } else {
-    	echo "The email message was not sent.";
-    }
+
+    if (isset($_POST['sumbit'])){
+        $name = $_POST['contact-name'];
+        $email = $_POST['contact-email'];
+        $phone = $_POST['contact-phone'];
+        $message = $_POST['contact-message'];
+
+
+        $to = "patrickmcalinden226@gmail.com";
+        $from = $email;
+        $subject = "Resume reachout";
+        $message = $message;
+        $headers = "From:" . $from;
+            if(mail($to,$subject,$message, $headers)) {
+		        echo "The email message was sent.";
+            } else {
+    	        echo "The email message was not sent.";
+
+}
 ?>
